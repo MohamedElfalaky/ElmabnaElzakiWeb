e63d2b29b1ba3e3a04f8311bdccadc55a7923c36
Mohamed Elfalaky <106435178+MohamedElfalaky@users.noreply.github.com>
2023-05-04 15:52:33 +0300

    lf

--- .dart_tool/flutter_build/c72c6e6149056c379540743392607228/web_resources.d
+++
@@ -1,1 +0,0 @@
- build/web/index.html build/web/favicon.png build/web/main-icon.png build/web/manifest.json:  /Users/mohamedelfalaky/Desktop/My\ Projects/ZahSoftWeb/web/index.html /Users/mohamedelfalaky/Desktop/My\ Projects/ZahSoftWeb/web/favicon.png /Users/mohamedelfalaky/Desktop/My\ Projects/ZahSoftWeb/web/main-icon.png /Users/mohamedelfalaky/Desktop/My\ Projects/ZahSoftWeb/web/manifest.json
